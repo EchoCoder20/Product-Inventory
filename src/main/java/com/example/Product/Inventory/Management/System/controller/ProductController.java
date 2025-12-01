45ffcb7cee3ce4b0ae43e176312fddd6d4f566bc
EchoCoder20 <albusaidi1915@gmail.com>
2025-12-01 11:15:08 +0400

    Add delete and update pages and process

--- src/main/java/com/example/Product/Inventory/Management/System/controller/ProductController.java
+++ src/main/java/com/example/Product/Inventory/Management/System/controller/ProductController.java
@@ -4,6 +4,7 @@ package com.example.Product.Inventory.Management.System.controller;
 import com.example.Product.Inventory.Management.System.entities.Category;
 import com.example.Product.Inventory.Management.System.entities.Product;
 import com.example.Product.Inventory.Management.System.entities.RequestProductDTO;
+import com.example.Product.Inventory.Management.System.entities.UpdateProductDTO;
 import com.example.Product.Inventory.Management.System.services.CategoryServices;
 import com.example.Product.Inventory.Management.System.services.ProductServices;
 import jakarta.validation.Valid;
@@ -13,9 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -59,4 +58,47 @@ public class ProductController {
         return "redirect:/";
 
     }
+    @GetMapping("/updatePage/{id}")
+    public String updatePage(Model model, @PathVariable int id) throws Exception {
+        Product product=productServices.findById(id);
+        UpdateProductDTO productDTO = productServices.convertToUpdateDTO(product);
+        model.addAttribute("products", productDTO); // for form binding
+        List<Category> categoryList=categoryServices.getAllCategory();
+
+        model.addAttribute("categories",categoryList);
+        if (!model.containsAttribute("errors")) {
+            model.addAttribute("errors", new ArrayList<String>());
+        }
+        return "update";
+    }
+    @PostMapping("/updateProduct")
+    public String updateProduct(@Valid @ModelAttribute UpdateProductDTO productDTO,BindingResult result,RedirectAttributes redirectAttributes){
+        if(result.hasErrors()){
+            List<String> errors=result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+            redirectAttributes.addFlashAttribute("errors",errors);
+            int productId=productDTO.getId();
+            return "redirect:/updatePage/"+productId;
+        }
+        productServices.updateProduct(productDTO);
+        redirectAttributes.addFlashAttribute("message","Product Update Successfully!");
+        return "redirect:/";
+    }
+    @GetMapping("delete/{id}")
+    public String deleteProduct(@PathVariable int id,RedirectAttributes redirectAttributes) throws Exception {
+        productServices.deleteProduct(id);
+        redirectAttributes.addFlashAttribute("message","Product UnActive Successfully!");
+        return "redirect:/";
+    }
+    @GetMapping("getById")
+    public String getProductById(Model model, @RequestParam int id) throws Exception {
+        Product product =productServices.findById(id);
+        model.addAttribute("products",product);
+        return "result";
+
+    }
+
+
 }
--- src/main/java/com/example/Product/Inventory/Management/System/services/ProductServices.java
+++ src/main/java/com/example/Product/Inventory/Management/System/services/ProductServices.java
@@ -3,11 +3,16 @@ package com.example.Product.Inventory.Management.System.services;
 import com.example.Product.Inventory.Management.System.entities.Category;
 import com.example.Product.Inventory.Management.System.entities.Product;
 import com.example.Product.Inventory.Management.System.entities.RequestProductDTO;
+import com.example.Product.Inventory.Management.System.entities.UpdateProductDTO;
 import com.example.Product.Inventory.Management.System.repositories.CategoryRepositories;
 import com.example.Product.Inventory.Management.System.repositories.ProductRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.PublicKey;
 import java.util.Date;
 import java.util.List;
 
@@ -33,4 +38,46 @@ public class ProductServices {
 
         return productRepositories.save(product);
     }
+    public Product findById(int id) throws Exception{
+        Product products=productRepositories.findById(id).get();
+        if(products !=null){
+            return products;
+        }else{
+            throw new Exception("Bad Request");
+        }
+    }
+    public UpdateProductDTO  convertToUpdateDTO(Product product){
+        UpdateProductDTO dto = new UpdateProductDTO();
+
+        dto.setId(product.getId());
+        dto.setName(product.getName());
+        dto.setPrice(product.getPrice());
+        dto.setQuantity(product.getQuantity());
+        dto.setCategoryId(product.getCategory().getId());
+
+        return dto;
+    }
+
+    public Product updateProduct(UpdateProductDTO productDTO){
+        Product product=productRepositories.findById(productDTO.getId()).get();
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        Category category=categoryRepositories.findById(productDTO.getCategoryId()).get();
+        product.setCategory(category);
+        product.setQuantity(productDTO.getQuantity());
+        product.setUpdatedData(new Date());
+        product.setActive(product.isActive());
+        return productRepositories.save(product);
+    }
+    public void deleteProduct(int id) throws Exception{
+        Product product=productRepositories.findById(id).get();
+        if(product!=null){
+            product.setUpdatedData(new Date());
+            product.setActive(false);
+            productRepositories.save(product);
+        }else{
+            throw new Exception("Bad Request");
+        }
+    }
+
 }
